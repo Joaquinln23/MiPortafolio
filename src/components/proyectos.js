45237a6e48ff53fd835f05422bdae2a037632180
Joaquín Leiva <jo.leivan@duocuc.cl>
2026-05-02 15:35:34 -0400

    fix(proyectos): remove unused FaStar import to fix build error

--- src/components/proyectos.js
+++ src/components/proyectos.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { FaStar } from 'react-icons/fa';
 import '../style/Proyectos.css';
 
 const PRIVATE_PROJECTS = [
